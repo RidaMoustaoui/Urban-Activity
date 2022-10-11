14420753cb96535c5a3377d2906d3d0c3a06cc9e
RadyAbdel <62503556+RadyAbdel@users.noreply.github.com>
2022-10-11 05:43:10 -0400

    login

--- verif_login.php
+++ verif_login.php
@@ -1,5 +1,22 @@
 <?php
 
-echo $_POST['login'] .' '. $_POST['mdp'];
+$mail = $_POST['login'];
+$mdp = $_POST['mdp'];
 
-?>
+include('connect.php');
+
+try {
+    $sql = "SELECT Mail, Mdp FROM users WHERE Mail=".$mail." AND Mdp=".$mdp ;
+    $res = mysqli_query($conn, $sql);
+
+    if (!$res) {
+        printf("Error: %s\n", mysqli_error($conn));
+        exit();
+    }
+
+    // Tableau numérique
+    $row = mysqli_fetch_array($res, MYSQLI_NUM);
+    echo $row[0] . "yo";
+} catch (Exception $e) {
+    echo $e;
+}
