19304b55db05b3148a5b1ed58e65d6c76dcd6998
RadyAbdel <62503556+RadyAbdel@users.noreply.github.com>
2022-10-11 12:24:49 -0400

    suppr page get act

--- house.php
+++ house.php
@@ -21,7 +21,38 @@
   
         <!-- Affichier des activités dynamiquement -->
         <?php
-          include('get_act.php');
+          include('connect.php');
+
+          try {
+              $sql = "SELECT * FROM activity";
+              $res = mysqli_query($conn, $sql);
+          
+              if (!$res) {
+                  printf("Error: %s\n", mysqli_error($conn));
+                  exit();
+              }
+          
+              $row = mysqli_fetch_array($res, MYSQLI_NUM);
+          
+          } catch (Exception $e) {
+              echo $e;
+          }
+          
+          try {
+              $sql1 = "SELECT COUNT(ActId) FROM activity";
+              $res1 = mysqli_query($conn, $sql1);
+          
+              if (!$res1) {
+                  printf("Error: %s\n", mysqli_error($conn));
+                  exit();
+              }
+          
+              $row1 = mysqli_fetch_array($res1, MYSQLI_NUM);
+          
+          } catch (Exception $e) {
+              echo $e;
+          }
+          
           $i=1;
           for($i;$i<=$row1[0];$i++)
           {
