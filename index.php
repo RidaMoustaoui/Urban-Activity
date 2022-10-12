16dc31d03dd397f825969966b58692b8ab1a30f6
RadyAbdel <62503556+RadyAbdel@users.noreply.github.com>
2022-10-12 10:29:58 -0400

    recup

--- index.php
+++ index.php
@@ -53,22 +53,36 @@
               $res2 = mysqli_query($conn, $sql2);
                 while($row2=mysqli_fetch_array($res2))
                 {
-                echo '
-                <div class="box">
-                <div class="img-box">
-                  <a href="'.$row[3].'" target="_blank">
-                  <img src="'.$row[4].'" alt="" width="200px" height="350px">
-                  </a>
-                </div>
-                <div class="detail-box">
-                  <h6>
-                    '.$row[1].'
-                  </h6>
-                  <p>
-                    '.$row[2].' - Créé par '.$row2[0]. ' '.$row2[1].'
-                  </p>
-                </div>
-              </div>';
+
+                  /* "Select COUNT(*) from participe where ActID=" $row[0] " NOMBRE DE PARTICIPANTS*/
+                  /* "Select MaxPart from activity where ActID=" $row[0] " NOMBRE MAX*/
+                  $sql3="SELECT COUNT(*) FROM participe WHERE ActId=".$row[0];
+                  $res3=mysqli_query($conn, $sql3);
+                  $sql4="SELECT MaxPart from activity WHERE ActId=".$row[0];
+                  $res4=mysqli_query($conn,$sql4);
+                  while($row3=mysqli_fetch_array($res3))
+                  {
+                      while($row4=mysqli_fetch_array($res4))
+                      {
+                        echo '
+                        <div class="box">
+                        <div class="img-box">
+                          <a href="'.$row[3].'" target="_blank">
+                          <img src="'.$row[4].'" alt="" width="200px" height="350px">
+                          </a>
+                        </div>
+                        <div class="detail-box">
+                          <h6>
+                            '.$row[1].' 
+                          </h6>
+                          <p>
+                            '.$row[2].' - Créé par '.$row2[0]. ' '.$row2[1].'
+                          </p>
+                        </div>
+                      </div>';
+                      }
+                  }
+                
                 }
               }
           
@@ -82,276 +96,7 @@
           Toutes les activités
         </a>
       </div>
-    
   </section>
-
-  <!-- end sale section -->
-
-  <!-- price section -->
-
-   <!--<section class="price_section layout_padding-bottom">
-    <div class="container">
-      <div class="heading_container">
-        <h2>
-          Our Pricing
-        </h2>
-        <p>
-          There are many variations of passages of Lorem Ipsum available, but the
-        </p>
-      </div>
-      <div class="price_container">
-        <div class="box">
-          <div class="detail-box">
-            <div class="heading-box">
-              <h4>
-                01
-              </h4>
-              <h6>
-                Basic
-              </h6>
-            </div>
-            <div class="text-box">
-              <h2>
-                $1000.00
-              </h2>
-              <ul>
-                <li>
-                  variations of
-                </li>
-                <li>
-                  passages of Lorem
-                </li>
-                <li>
-                  Ipsum available,
-                </li>
-                <li>
-                  but the majority
-                </li>
-                <li>
-                  have suffered
-                </li>
-                <li>
-                  alteration in
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="btn-box">
-            <a href="">
-              Buy Now
-            </a>
-          </div>
-        </div>
-        <div class="box">
-          <div class="detail-box">
-            <div class="heading-box">
-              <h4>
-                02
-              </h4>
-              <h6>
-                Standard
-              </h6>
-            </div>
-            <div class="text-box">
-              <h2>
-                $2000.00
-              </h2>
-              <ul>
-                <li>
-                  variations of
-                </li>
-                <li>
-                  passages of Lorem
-                </li>
-                <li>
-                  Ipsum available,
-                </li>
-                <li>
-                  but the majority
-                </li>
-                <li>
-                  have suffered
-                </li>
-                <li>
-                  alteration in
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="btn-box">
-            <a href="">
-              Buy Now
-            </a>
-          </div>
-        </div>
-        <div class="box">
-          <div class="detail-box">
-            <div class="heading-box">
-              <h4>
-                03
-              </h4>
-              <h6>
-                Premium
-              </h6>
-            </div>
-            <div class="text-box">
-              <h2>
-                $3000.00
-              </h2>
-              <ul>
-                <li>
-                  variations of
-                </li>
-                <li>
-                  passages of Lorem
-                </li>
-                <li>
-                  Ipsum available,
-                </li>
-                <li>
-                  but the majority
-                </li>
-                <li>
-                  have suffered
-                </li>
-                <li>
-                  alteration in
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="btn-box">
-            <a href="">
-              Buy Now
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>-->
-  <!-- end price section -->
-
-  <!-- deal section -->
-
-  <!--<section class="deal_section layout_padding-bottom">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="detail-box">
-            <div class="heading_container">
-              <h2>
-                Very Good Deal For House
-              </h2>
-            </div>
-            <p>
-              There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration
-              in
-              some form, by injected humour, or randomised words which don't look even slightly believable.
-            </p>
-            <a href="">
-              Get A Quote
-            </a>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="img-box">
-            <div class="box b1">
-              <img src="images/d-1.jpg" alt="">
-            </div>
-            <div class="box b1">
-              <img src="images/d-2.jpg" alt="">
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section> -->
-
-  <!-- end deal section -->
-
-
-  <!-- us section -->
-    <!--<section class="us_section layout_padding2">
-
-    <div class="container">
-      <div class="heading_container">
-        <h2>
-          Why Choose Us
-        </h2>
-      </div>
-      <div class="row">
-        <div class="col-md-3 col-sm-6">
-          <div class="box">
-            <div class="img-box">
-              <img src="images/u-1.png" alt="">
-            </div>
-            <div class="detail-box">
-              <h3 class="price">
-                1000+
-              </h3>
-              <h5>
-                Years of House
-              </h5>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3 col-sm-6">
-          <div class="box">
-            <div class="img-box">
-              <img src="images/u-2.png" alt="">
-            </div>
-            <div class="detail-box">
-              <h3>
-                20000+
-              </h3>
-              <h5>
-                Projects Delivered
-              </h5>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3 col-sm-6">
-          <div class="box">
-            <div class="img-box">
-              <img src="images/u-3.png" alt="">
-            </div>
-            <div class="detail-box">
-              <h3>
-                10000+
-              </h3>
-              <h5>
-                Satisfied Customers
-              </h5>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3 col-sm-6">
-          <div class="box">
-            <div class="img-box">
-              <img src="images/u-4.png" alt="">
-            </div>
-            <div class="detail-box">
-              <h3>
-                1500+
-              </h3>
-              <h5>
-                Cheap Rates
-              </h5>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="btn-box">
-        <a href="">
-          Get A Quote
-        </a>
-      </div>
-    </div>
-  </section> -->
-
-  <!-- end us section -->
-
-  <!-- client secction -->
 
   <section class="client_section" style="padding-bottom:90px">
     <div class="container-fluid">
