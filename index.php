352fdefce484976a1bb21d9332aadfafcb9adde4
Rida <moustaoui.rida@gmail.com>
2022-10-12 11:23:54 +0200

    add features

--- index.php
+++ index.php
@@ -425,7 +425,7 @@
             <div class="carousel-item">
               <div class="box">
                 <div class="img-box">
-                  <img src="William.jpeg" alt="Bilel" width="100" height="143">
+                  <img src="images.png" alt="Bilel" width="100" height="143">
                 </div>
                 <div class="detail-box">
                   <h5>
