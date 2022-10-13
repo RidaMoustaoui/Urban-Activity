9ffbf79a6242a54100d24f7ec82d0de56f623952
RadyAbdel <62503556+RadyAbdel@users.noreply.github.com>
2022-10-13 13:52:23 -0400

    Update participe.php

--- participe.php
+++ participe.php
@@ -1,35 +1,29 @@
 <?php
-    if(!isset($_SESSION['Mail']))
-    {
-      session_start();
-    }
+if (!isset($_SESSION['Mail'])) {
+    session_start();
+}
 
-    include('connect.php');
+include('connect.php');
 
-try
-{   
-    if($_POST['getPar'] < $_POST['getMax'])
-    {
-        $sql = "INSERT INTO participe (UserID, ActId) VALUES ('".$_SESSION['UserID']."', '".$_POST['getAct']."');";
-        if(mysqli_query($conn, $sql))
-        {
-            echo "Vous avez participé à l'activité";
-            include('house.php');
-        }
-        else
-        {
+try {
+    if ($_POST['getMax'] > 0 or $_POST['getAct'] > 0) {
+        if ($_POST['getPar'] < $_POST['getMax']) {
+            $sql = "INSERT INTO participe (UserID, ActId) VALUES ('" . $_SESSION['UserID'] . "', '" . $_POST['getAct'] . "');";
+            if (mysqli_query($conn, $sql)) {
+                echo "Vous avez participé à l'activité";
+                include('house.php');
+            } else {
+                echo "erreur";
+                header("Location: house.php");
+            }
+        } else {
             echo "erreur";
-            include('house.php');
+            header("Location: house.php");
         }
-    }else
-    {
+    } else {
         echo "erreur";
-        include('house.php');
+        header("Location: house.php");
     }
-    
-    
 } catch (Exception $e) {
     echo $e;
 }
-
-?>
